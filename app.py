e54fd22780d7c35e4e2168253af349de699d09cf
Neel Patel <ec2-user@ip-172-31-95-194.ec2.internal>
2021-01-31 21:52:40 +0000

    first

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
-for i in range(5):
-    print(i)
-print("hello world")
+import os
+from flask import Flask,render_template
+
+app=Flask(__name__)
+
+@app.route('/')
+def homepage():
+    artist=["Arijit Singh","Ed Sheeran","Ariana Grande"]
+    return render_template(
+        "index.html",
+         len= len(artist),
+         artist = artist
+        )
+app.run(
+    port=int(os.getenv('PORT',8080)),
+    host=os.getenv('IP','0.0.0.0'),
+    use_reloader = True,
+    debug = True
+    )
