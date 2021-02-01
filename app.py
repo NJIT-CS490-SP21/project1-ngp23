db54d94aa2d6c3e5072b4874278433995d5658a9
Neel Patel <ec2-user@ip-172-31-95-194.ec2.internal>
2021-02-01 00:36:28 +0000

    Update

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask,render_template
 
 app=Flask(__name__)
-
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 @app.route('/')
 def homepage():
     artist=["Arijit Singh","Ed Sheeran","Ariana Grande"]
@@ -15,5 +15,5 @@ app.run(
     port=int(os.getenv('PORT',8080)),
     host=os.getenv('IP','0.0.0.0'),
     use_reloader = True,
-    debug = True
+    debug = True,
     )
