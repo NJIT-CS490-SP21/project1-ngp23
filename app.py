57c93fb2ac9c35adb8768897d25866f8c8963778
Neel Patel <ec2-user@ip-172-31-95-194.ec2.internal>
2021-02-03 15:28:15 +0000

    updated

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 @app.route('/')
 def homepage():
    
-    artist={'Arijit Singh':'https://open.spotify.com/artist/4YRxDV8wJFPHPTeXepOstw',
-        'Ed Sheeran':'https://open.spotify.com/artist/6eUKZXaKkcviH0Ku9w2n3V',
-        'Ariana Grande':'https://open.spotify.com/artist/66CXWjxzNUsdJxJ2JdwvnR',
-        'Jubin Nautiyal':'https://open.spotify.com/artist/1tqysapcCh1lWEAc9dIFpa',
-        'Darshan Raval':'https://open.spotify.com/artist/2GoeZ0qOTt6kjsWW4eA6LS'
+    artist={'Arijit Singh':"https://open.spotify.com/artist/4YRxDV8wJFPHPTeXepOstw",
+        'Ed Sheeran':"https://open.spotify.com/artist/6eUKZXaKkcviH0Ku9w2n3V",
+        'Ariana Grande':"https://open.spotify.com/artist/66CXWjxzNUsdJxJ2JdwvnR",
+        'Jubin Nautiyal':"https://open.spotify.com/artist/1tqysapcCh1lWEAc9dIFpa",
+        'Darshan Raval':"https://open.spotify.com/artist/2GoeZ0qOTt6kjsWW4eA6LS"
         }
     rand = random.randint(0,len(artist))
     randk=random.choice(list(artist.keys()))
