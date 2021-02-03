e6da65ac7896cf339f9f0aa521d9a31135e93ced
Neel Patel <ec2-user@ip-172-31-95-194.ec2.internal>
2021-02-03 01:26:04 +0000

    Updated the files

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
-import os
+import os,random
 from flask import Flask,render_template
 
 app=Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 @app.route('/')
 def homepage():
-    artist=["Arijit Singh","Ed Sheeran","Ariana Grande","Jubin Nautiyal","Darshan Raval"]
+    a=["Arijit Singh","Ed Sheeran","Ariana Grande","Jubin Nautiyal","Darshan Raval"]
+    artist={'Arijit Singh':'https://open.spotify.com/artist/4YRxDV8wJFPHPTeXepOstw',
+        'Ed Sheeran':'https://open.spotify.com/artist/6eUKZXaKkcviH0Ku9w2n3V',
+        'Ariana Grande':'https://open.spotify.com/artist/66CXWjxzNUsdJxJ2JdwvnR',
+        'Jubin Nautiyal':'https://open.spotify.com/artist/1tqysapcCh1lWEAc9dIFpa',
+        'Darshan Raval':'https://open.spotify.com/artist/2GoeZ0qOTt6kjsWW4eA6LS'
+        }
+    rand = random.randint(0,len(artist))
     return render_template(
         "index.html",
-         len= len(artist),
-         artist = artist
+         len = len(artist),
+         artist = artist,
+         a=a,
+         rand=rand
         )
 app.run(
     port=int(os.getenv('PORT',8080)),
